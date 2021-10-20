dbf147c5c9c54bf1dd881b5ca16ce1107e0cc5a4
Juanma L <juanmalubian@gmail.com>
2021-10-20 10:43:49 +0200

    Commit inicial

---
+++ src/main/java/com/nttdata/bootcamp/Saludo.java
@@ -0,0 +1,28 @@
+package com.nttdata.bootcamp;
+
+public class Saludo {
+	private String idioma;
+	
+	public Saludo(String idioma) {
+		this.idioma = idioma;
+	}
+	
+	public String mensajeSaludo() {
+		String mensaje = "";
+		switch (idioma) {
+		case "Castellano":
+			mensaje = "Bienvenido";
+			break;
+		case "Ingles":
+			mensaje = "Welcome";
+			break;
+		case "Italiano":
+			mensaje = "Benvenuto";
+			break;
+		default:
+			mensaje = "Bienvenido";
+			break;
+		}
+		return mensaje;
+	}
+}
